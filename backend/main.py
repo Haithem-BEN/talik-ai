70b4bf038d9250d50b6b593b9ddd62e5f009f4ec
Haithem-BEN <haithamofficial98@gmail.com>
2023-06-05 02:02:36 +0100

    settuing the env vars and the AI instructions and the database

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,51 @@
-from fastapi import FastAPI
+#uvicorn main:app --reload
 
+
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
+import openai
+
+
+# Custom Functions IMports
+from functions.openai_requests import convert_audio_to_text
+
+
+# Initiate APP
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello Worlds"}
+# CORS - Origins
+origins = [
+    "http://127.0.0.1:3000"
+]
+
+
+# CORS - Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Check Health 
+@app.get("/health")
+async def check_health():
+    return {"message": "healthy"}
+
+
+# Post bot response
+@app.get("/post-audio/")
+async def post_audio():
+    audio_input = open("voice.mp3", "rb")
+
+    # Decode Audio 
+    message_decoded = convert_audio_to_text(audio_input)
+    
+    print(message_decoded)
+
+    return "Done"
